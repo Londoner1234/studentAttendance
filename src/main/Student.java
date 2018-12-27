7d457c3177397287f4833fa9010178ab904317ed
ahv15 <ahv15@IREL21BZPQ2.mshome.net>
2018-12-27 15:37:47 +0000

    Adding in comments

--- src/main/Student.java
+++ src/main/Student.java
@@ -1,7 +1,11 @@
 /**
  * Created by meudecc on 15/02/2017.
  * Modified 
- * -Thomas Woodhouse 
+ * -Thomas Woodhouse
+ * Log 
+ * Added line 15 to check equal to 0 
+ * Removed line 18 to check < 50  
+ * Changed line 20 from <= to < 
  */
 public class Student {
 
@@ -9,10 +13,11 @@ public class Student {
 
     public AttendanceGrade getAttendanceGrade(int attendance) {
         if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
+        if (attendance == 0) return AttendanceGrade.ABSENT; //Added this Line
         if (attendance < 30) return AttendanceGrade.VERY_POOR;
-        if (attendance < 50) return AttendanceGrade.POOR;
+        //if (attendance < 50) return AttendanceGrade.POOR;  //Removed this line
         if (attendance < 70) return AttendanceGrade.AVERAGE;
-        if (attendance <= 90) return AttendanceGrade.GOOD;
+        if (attendance < 90) return AttendanceGrade.GOOD; //Change from <= to < 
         return AttendanceGrade.VERY_GOOD;
     }
 }
